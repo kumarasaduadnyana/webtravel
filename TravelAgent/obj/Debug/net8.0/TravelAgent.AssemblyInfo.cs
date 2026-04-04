de247ec12fa16e3fbfba9112bf651179bd0220f2
kumarasaduadnyana <kumara@travlr.com>
2026-04-04 13:06:16 +0800

    update background UI

--- TravelAgent/obj/Debug/net8.0/TravelAgent.AssemblyInfo.cs
+++ TravelAgent/obj/Debug/net8.0/TravelAgent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a171b2e3f74c150ed319cdb682c68d82fca632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c25ac2ee4cbf83d25d4e73d9b17deaffe3f6a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
