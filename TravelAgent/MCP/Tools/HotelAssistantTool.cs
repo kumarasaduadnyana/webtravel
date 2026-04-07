989f3d71bf0776378e2c133c5778f72ad75a7e58
kumarasaduadnyana <kumara@travlr.com>
2026-04-07 17:10:49 +0800

    add details page tools

--- TravelAgent/MCP/Tools/HotelAssistantTool.cs
+++ TravelAgent/MCP/Tools/HotelAssistantTool.cs
@@ -1,7 +1,8 @@
-﻿using System.ComponentModel;
-using ModelContextProtocol.Server;
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
 using TravelAgent.Models;
 using TravelAgent.Services;
+using HotelDetail = TravelAgent.Models.HotelDetail;
 
 namespace TravelAgent.MCP.Tools;
 
@@ -42,6 +43,41 @@ public class HotelAssistantTool(IHotelService hotelService)
                 MinRating = minRating,
                 Amenities = amenities
             }
+        };
+    }
+
+    // TODO create tools for search detail page
+    [McpServerTool(Name = "get_hotels_detail", Idempotent = true, OpenWorld = false, UseStructuredContent = true)]
+    [Description("Search for detail hotel")]
+    [McpMeta("ui", JsonValue = """{"resourceUri": "ui://hotel-detail-widget.html"}""")]
+    [McpMeta("ui/resourceUri", "ui://hotel-detail-widget.html")]
+    public async Task<HotelDetail> SearchDetailHotel(
+        [Description("Hotel Id")] string hotel_id,
+        [Description("Hotel Code")] string? hotel_code,
+        [Description("Provider")] string? provider,
+        [Description("Check In")] DateTime check_in,
+        [Description("Check Out")] DateTime check_out,
+        [Description("Adult")] int adults = 2,
+        [Description("Room")] int rooms = 1,
+        [Description("Default Currency")] string currency = "AUD"
+        )
+    {
+        var searchHotelDetail = await hotelService.GetHotelDetails(hotel_id, hotel_code, provider, check_in, check_out, adults, rooms, currency);
+
+        return new HotelDetail
+        {
+            Address = searchHotelDetail?.Address ?? string.Empty,
+            Amenities = searchHotelDetail?.Amenities,
+            Currency = searchHotelDetail?.Currency ?? string.Empty,
+            Description = searchHotelDetail?.Description ?? string.Empty,
+            Id = searchHotelDetail?.Id,
+            Images = searchHotelDetail?.Images,
+            Latitude = searchHotelDetail?.Latitude,
+            Longitude = searchHotelDetail?.Longitude,
+            Name = searchHotelDetail?.Name,
+            Price = searchHotelDetail?.Price,
+            RoomRates = searchHotelDetail.RoomRates,
+            StarRating = searchHotelDetail.StarRating
         };
     }
 }
